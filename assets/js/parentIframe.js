fce89cb78494acaebbb7e94b7471a514d6e4dbb9
ashish sharma <ashish.sharma@beamstacks.com>
2022-06-02 08:50:54 +0530

    fixing event issue

--- assets/js/parentIframe.js
+++ assets/js/parentIframe.js
@@ -16,86 +16,68 @@ window.addEventListener('message', function (eventData) {
         let parsedData = JSON.parse(eventData.data)
         if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "all_lables") {
             console.log("\n\n\n <--- All products received in parent iframe ---> \n\n\n", parsedData);
-            document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event_code: 'custom-parent-client-event',
                 data: parsedData.data.data
-            }), '*');
+            }), true);
             return;
         }
 
         if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "applied_coupons_YM") {
             console.log("\n\n\n <--- Applied coupons received in parent iframe ---> \n\n\n", parsedData);
-            document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event_code: 'custom-parent-client-checkout-event',
                 data: parsedData.data.data
-            }), '*');
+            }), true);
             return;
         }
 
         if (parsedData?.event_code == 'custom-event' && parsedData?.data?.code == "recent_order_YM") {
             console.log("\n\n\n <--- Recent order data received in parent iframe ---> \n\n\n", parsedData);
-            document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event_code: 'custom-parent-client-recent-order-event',
                 data: parsedData.data.data
-            }), '*');
+            }), true);
             return;
         }
 
         if (parsedData?.event_code == 'custom-parenttoroot-client-event' && parsedData?.data) {
             console.log("\n\n\n <--- Applied coupons received in parent iframe ---> \n\n\n", parsedData);
-            document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event_code: 'ym-client-event',
                 data: {
                     event: {
-                     code: "updated-json",
-                     data: parsedData
+                        code: "updated-json",
+                        data: parsedData
                     }
                 }
-           }), '*');
-            window.location.href= 'https://wa.me/+94773233440?text=continue';
+            }), true);
+            window.location.href = 'https://wa.me/+94773233440?text=continue';
             return;
         }
-        
-        
-    const ymIfrmae = document.getElementById('ymIframe');
-        
 
-     try {
-            if (parsedData?.event_code == 'custom-parenttoroot-checkout-event') {
+        if (parsedData?.event_code == 'custom-parenttoroot-checkout-event') {
             console.log("\n\n\n <--- Checkout event received in parent iframe ---> \n\n\n", parsedData);
-               window.frames.ymIframe.chat.send({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event: {
-                 code: "applied_coupons",
-                 data: parsedData
+                    code: "applied_coupons",
+                    data: parsedData
                 }
-            }, true);
-//             ymIfrmae.contentWindow.postMessage(JSON.stringify({
-//                 event_code: 'ym-client-event',
-//                 data: {
-//                     event: {
-//                      code: "applied_coupons",
-//                      data: parsedData
-//                     }
-//                 }
-//             }), '*');
-            console.log('send applied coupons Event Successfully')
+            }), true);
             return;
         }
-     }catch(e) {
-        console.log(e, 'Error in applied coupons Events')
-     }
-
+        
         if (parsedData?.event_code == 'custom-parenttoroot-recent-order-event') {
             console.log("\n\n\n <--- Fetch recent orders received in parent iframe ---> \n\n\n", parsedData);
-            document.getElementById('ymIframe').contentWindow.postMessage(JSON.stringify({
+            window.frames.ymIframe.chat.send(JSON.stringify({
                 event_code: 'ym-client-event',
                 data: {
                     event: {
-                     code: "fetch_recent_orders",
-                     data: parsedData
+                        code: "fetch_recent_orders",
+                        data: parsedData
                     }
                 }
-           }), '*');
+            }), true);
             return;
         }
 
